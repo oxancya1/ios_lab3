3bcde9e24659cba6afe95db27131e6f6b535aeaf
oxancya1 <136077611+oxancya1@users.noreply.github.com>
2024-11-20 22:34:48 +0200

    Add files via upload

---
+++ lab3/lab3/lab3/lab3App.swift
@@ -0,0 +1,17 @@
+//
+//  lab3App.swift
+//  lab3
+//
+//  Created by IPZ-31 on 03.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct lab3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
